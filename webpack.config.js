2312fedbc732ff82b75cff51d8dd9cffb4bf39c0
vinodronold <vinodronoldbe@gmail.com>
2017-12-16 00:36:02 -0500

    Fixing Travis Webpack Build issue

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'lib'),
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
@@ -14,7 +14,7 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: /(node_modules|bower_components|lib)/,
         use: {
           loader: 'babel-loader',
           options: {
