da676d702b886bbd3eb059db3fc7dfd8bd818619
vinodronold <vinodronoldbe@gmail.com>
2017-12-19 00:01:08 -0500

    Updating Jest to 22 Refactoring guitarChordShape

--- src/Guitar/guitarChordShape.js
+++ src/Guitar/guitarChordShape.js
@@ -9,111 +9,39 @@
  *  }
  * }
  */
-
-const barreCommonEMajShape = s => ({
-  b: 1,
-  s,
-  p: [0, 3, 3, 2, 0, 0]
-})
-const barreCommonEMinShape = s => ({
-  b: 1,
-  s,
-  p: [0, 3, 3, 0, 0, 0]
-})
-const barreCommonAMajShape = s => ({
-  b: 1,
-  s,
-  p: [0, 0, 3, 3, 3, 0]
-})
-const barreCommonAMinShape = s => ({
-  b: 1,
-  s,
-  p: [0, 0, 3, 3, 2, 0]
-})
-
-const barreCommonEShape = s => ({
-  MAJ: {
-    b: 1,
-    s,
-    p: [0, 3, 3, 2, 0, 0]
-  },
-  MIN: {
-    b: 1,
-    s,
-    p: [0, 3, 3, 0, 0, 0]
-  }
-})
-const barreCommonAShape = s => ({
-  MAJ: {
-    b: 1,
-    s,
-    p: [0, 0, 3, 3, 3, 0]
-  },
-  MIN: {
-    b: 1,
-    s,
-    p: [0, 0, 3, 3, 2, 0]
-  }
-})
+const chordCommon = p => b => s => ({ b, s, p })
+const barreCommonEMajShape = chordCommon([0, 3, 3, 2, 0, 0])(1)
+const barreCommonEMinShape = chordCommon([0, 3, 3, 0, 0, 0])(1)
+const barreCommonAMajShape = chordCommon([0, 0, 3, 3, 3, 0])(1)
+const barreCommonAMinShape = chordCommon([0, 0, 3, 3, 2, 0])(1)
+const chordMajMin = (MAJ, MIN) => ({ MAJ, MIN })
 
 export default {
-  A: {
-    MAJ: {
-      b: 0,
-      p: [0, 0, 2, 2, 2, 0]
-    },
-    MIN: {
-      b: 0,
-      p: [0, 0, 2, 2, 1, 0]
-    }
-  },
-  'A#': barreCommonAShape(1),
-  B: barreCommonAShape(2),
-  C: {
-    MAJ: {
-      b: 0,
-      p: [0, 3, 2, 0, 1, 0]
-    },
-    MIN: {
-      b: 1,
-      s: 3,
-      p: [0, 0, 3, 3, 2, 0]
-    }
-  },
-  'C#': barreCommonAShape(4),
-  D: {
-    MAJ: {
-      b: 0,
-      p: [0, 0, 0, 2, 3, 2]
-    },
-    MIN: {
-      b: 0,
-      p: [0, 0, 0, 2, 3, 1]
-    }
-  },
-  'D#': barreCommonAShape(6),
-  E: {
-    MAJ: {
-      b: 0,
-      p: [0, 2, 2, 1, 0, 0]
-    },
-    MIN: {
-      b: 0,
-      p: [0, 2, 2, 0, 0, 0]
-    }
-  },
-  F: barreCommonEShape(1),
-  'F#': barreCommonEShape(2),
-  G: {
-    MAJ: {
-      b: 0,
-      p: [3, 2, 0, 0, 3, 3]
-    },
-    MIN: {
-      b: 1,
-      s: 3,
-      p: [0, 3, 3, 0, 0, 0]
-    }
-  },
-  'G#': barreCommonEShape(4)
+  A: chordMajMin(
+    chordCommon([0, 0, 2, 2, 2, 0])(0)(0),
+    chordCommon([0, 0, 2, 2, 1, 0])(0)(0)
+  ),
+  'A#': chordMajMin(barreCommonAMajShape(1), barreCommonAMinShape(1)),
+  B: chordMajMin(barreCommonAMajShape(2), barreCommonAMinShape(2)),
+  C: chordMajMin(
+    chordCommon([0, 3, 2, 0, 1, 0])(0)(0),
+    barreCommonAMinShape(3)
+  ),
+  'C#': chordMajMin(barreCommonAMajShape(4), barreCommonAMinShape(4)),
+  D: chordMajMin(
+    chordCommon([0, 0, 0, 2, 3, 2])(0)(0),
+    chordCommon([0, 0, 0, 2, 3, 1])(0)(0)
+  ),
+  'D#': chordMajMin(barreCommonAMajShape(6), barreCommonAMinShape(6)),
+  E: chordMajMin(
+    chordCommon([0, 2, 2, 1, 0, 0])(0)(0),
+    chordCommon([0, 2, 2, 0, 0, 0])(0)(0)
+  ),
+  F: chordMajMin(barreCommonEMajShape(1), barreCommonEMinShape(1)),
+  'F#': chordMajMin(barreCommonEMajShape(2), barreCommonEMinShape(2)),
+  G: chordMajMin(
+    chordCommon([3, 2, 0, 0, 3, 3])(0)(0),
+    barreCommonEMinShape(3)
+  ),
+  'G#': chordMajMin(barreCommonEMajShape(4), barreCommonEMinShape(4))
 }
