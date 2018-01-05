8cdd6b2a7f1670f09f94fe837d03b9156722f76d
Vinod Ronold <vinodronoldbe@gmail.com>
2018-01-05 16:17:14 -0500

    Added alternate chord diagram

--- src/Guitar/__test__/Guitar.test.js
+++ src/Guitar/__test__/Guitar.test.js
@@ -7,17 +7,39 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'A'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('A alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'A'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('Am should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'A'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('Am alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'A'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
+
   it('A#/Bb should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'A#'} />)
     expect(_guitar).toMatchSnapshot()
   })
-  it('A#m/Bb# should match the sanpshot', () => {
+  it('A#/Bb alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'A#'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+
+  it('A#m/Bbm should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'A#'} quality={'MIN'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('A#m/Bbm alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'A#'} quality={'MIN'} />
+    )
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -25,9 +47,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'B'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('B alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'B'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('Bm should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'B'} quality={'MIN'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('Bm alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'B'} quality={'MIN'} />
+    )
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -35,9 +67,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'C'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('C alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'C'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('Cm should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'C'} quality={'MIN'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('Cm alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'C'} quality={'MIN'} />
+    )
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -50,9 +92,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'C#'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('C#m alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'C#'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('D should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'D'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('D alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'D'} />)
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -60,9 +112,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'D'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('Dm alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'D'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('D# should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'D#'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('D alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'D#'} />)
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -70,9 +132,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'D#'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('D#m alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'D#'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('E should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'E'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('E alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'E'} />)
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -80,9 +152,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'E'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('Em alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'E'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('F should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'F'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('F alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'F'} />)
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -90,9 +172,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'F'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('Fm alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'F'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('F# should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'F#'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('F# alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'F#'} />)
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -100,9 +192,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'F#'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('F#m alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'F#'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('G should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'G'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('G alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'G'} />)
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -110,9 +212,19 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'G'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('Gm alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'G'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('G# should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'G#'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+  it('G# alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(<Guitar alternate chord={'G#'} />)
     expect(_guitar).toMatchSnapshot()
   })
 
@@ -120,9 +232,20 @@ describe('Guitar', () => {
     const _guitar = renderer.create(<Guitar chord={'G#'} quality={'MIN'} />)
     expect(_guitar).toMatchSnapshot()
   })
+  it('G#m alternate should match the sanpshot', () => {
+    const _guitar = renderer.create(
+      <Guitar alternate chord={'G#'} quality={'MIN'} />
+    )
+    expect(_guitar).toMatchSnapshot()
+  })
 
   it('X# should match the sanpshot', () => {
     const _guitar = renderer.create(<Guitar chord={'X#'} />)
+    expect(_guitar).toMatchSnapshot()
+  })
+
+  it('A should match the sanpshot with height', () => {
+    const _guitar = renderer.create(<Guitar chord={'A'} height={'30em'} />)
     expect(_guitar).toMatchSnapshot()
   })
 })
--- src/Guitar/guitarChordShape.js
+++ src/Guitar/guitarChordShape.js
@@ -18,30 +18,51 @@ const chordMajMin = (MAJ, MIN) => ({ MAJ, MIN })
 
 export default {
   A: chordMajMin(
-    chordCommon([0, 0, 2, 2, 2, 0])(0)(0),
-    chordCommon([0, 0, 2, 2, 1, 0])(0)(0)
+    [chordCommon([0, 0, 2, 2, 2, 0])(0)(0), barreCommonEMajShape(5)],
+    [chordCommon([0, 0, 2, 2, 1, 0])(0)(0), barreCommonEMinShape(5)]
   ),
-  'A#': chordMajMin(barreCommonAMajShape(1), barreCommonAMinShape(1)),
-  B: chordMajMin(barreCommonAMajShape(2), barreCommonAMinShape(2)),
+  'A#': chordMajMin(
+    [barreCommonAMajShape(1), barreCommonEMajShape(6)],
+    [barreCommonAMinShape(1), barreCommonEMinShape(6)]
+  ),
+  B: chordMajMin(
+    [barreCommonAMajShape(2), barreCommonEMajShape(7)],
+    [barreCommonAMinShape(2), barreCommonEMinShape(7)]
+  ),
   C: chordMajMin(
-    chordCommon([0, 3, 2, 0, 1, 0])(0)(0),
-    barreCommonAMinShape(3)
+    [chordCommon([0, 3, 2, 0, 1, 0])(0)(0), barreCommonEMajShape(8)],
+    [barreCommonAMinShape(3), barreCommonEMinShape(8)]
   ),
-  'C#': chordMajMin(barreCommonAMajShape(4), barreCommonAMinShape(4)),
+  'C#': chordMajMin(
+    [barreCommonAMajShape(4), barreCommonEMajShape(9)],
+    [barreCommonAMinShape(4), barreCommonEMinShape(9)]
+  ),
   D: chordMajMin(
-    chordCommon([0, 0, 0, 2, 3, 2])(0)(0),
-    chordCommon([0, 0, 0, 2, 3, 1])(0)(0)
+    [chordCommon([0, 0, 0, 2, 3, 2])(0)(0), barreCommonAMajShape(5)],
+    [chordCommon([0, 0, 0, 2, 3, 1])(0)(0), barreCommonAMinShape(5)]
   ),
-  'D#': chordMajMin(barreCommonAMajShape(6), barreCommonAMinShape(6)),
+  'D#': chordMajMin(
+    [barreCommonAMajShape(6), barreCommonEMajShape(11)],
+    [barreCommonAMinShape(6), barreCommonEMinShape(11)]
+  ),
   E: chordMajMin(
-    chordCommon([0, 2, 2, 1, 0, 0])(0)(0),
-    chordCommon([0, 2, 2, 0, 0, 0])(0)(0)
+    [chordCommon([0, 2, 2, 1, 0, 0])(0)(0), barreCommonAMajShape(7)],
+    [chordCommon([0, 2, 2, 0, 0, 0])(0)(0), barreCommonAMinShape(7)]
   ),
-  F: chordMajMin(barreCommonEMajShape(1), barreCommonEMinShape(1)),
-  'F#': chordMajMin(barreCommonEMajShape(2), barreCommonEMinShape(2)),
+  F: chordMajMin(
+    [barreCommonEMajShape(1), barreCommonAMajShape(8)],
+    [barreCommonEMinShape(1), barreCommonAMinShape(8)]
+  ),
+  'F#': chordMajMin(
+    [barreCommonEMajShape(2), barreCommonAMajShape(9)],
+    [barreCommonEMinShape(2), barreCommonAMinShape(9)]
+  ),
   G: chordMajMin(
-    chordCommon([3, 2, 0, 0, 3, 3])(0)(0),
-    barreCommonEMinShape(3)
+    [chordCommon([3, 2, 0, 0, 3, 3])(0)(0), barreCommonEMajShape(3)],
+    [barreCommonEMinShape(3), barreCommonAMinShape(10)]
   ),
-  'G#': chordMajMin(barreCommonEMajShape(4), barreCommonEMinShape(4))
+  'G#': chordMajMin(
+    [barreCommonEMajShape(4), barreCommonAMajShape(11)],
+    [barreCommonEMinShape(4), barreCommonAMinShape(11)]
+  )
 }
